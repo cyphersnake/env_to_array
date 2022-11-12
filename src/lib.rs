e12dd06212ff9842419bfcb890370d2177365db7
Cyphersnake <cyphersnake@pm.me>
2022-11-12 15:05:13 +0400

    Remove build.rs and start work with compile time env setter

--- src/lib.rs
+++ src/lib.rs
@@ -2,10 +2,28 @@ use itertools::Itertools;
 use proc_macro::TokenStream;
 use syn::parse_macro_input;
 
+#[proc_macro]
+pub fn test_compile_time_env(_input: TokenStream) -> TokenStream {
+    std::env::set_var(
+        "__ENV_TO_ARRAY_HEX",
+        "5bb32685b7ea2d07382db3744d43df89f97148eeae559ec4d0d1feefa2ee789d",
+    );
+    std::env::set_var(
+        "__ENV_TO_ARRAY_BS64",
+        "W7MmhbfqLQc4LbN0TUPfiflxSO6uVZ7E0NH+76LueJ0=",
+    );
+    std::env::set_var(
+        "__ENV_TO_ARRAY_BS58",
+        "7AxYSahRegVSuU76JGWNxzdwVAPpaonY26V6JH17ToUQ",
+    );
+    TokenStream::new()
+}
+
 #[cfg(feature = "bs58")]
 /// Get from env variable string, decode it from bs58 and write array as result
 /// ```
-/// const ID: [u8; 32] = env_to_array::bs58_env_to_array!("_ENV_TO_ARRAY_BS58");
+/// env_to_array::test_compile_time_env!();
+/// const ID: [u8; 32] = env_to_array::bs58_env_to_array!("__ENV_TO_ARRAY_BS58");
 /// ```
 #[proc_macro]
 pub fn bs58_env_to_array(input: TokenStream) -> TokenStream {
@@ -27,7 +45,8 @@ pub fn bs58_env_to_array(input: TokenStream) -> TokenStream {
 #[cfg(feature = "bs64")]
 /// Get from env variable string, decode it from bs64 and write array as result
 /// ```
-/// const ID: [u8; 32] = env_to_array::bs64_env_to_array!("_ENV_TO_ARRAY_BS64");
+/// env_to_array::test_compile_time_env!();
+/// const ID: [u8; 32] = env_to_array::bs64_env_to_array!("__ENV_TO_ARRAY_BS64");
 /// ```
 #[proc_macro]
 pub fn bs64_env_to_array(input: TokenStream) -> TokenStream {
@@ -49,7 +68,8 @@ pub fn bs64_env_to_array(input: TokenStream) -> TokenStream {
 #[cfg(feature = "hex")]
 /// Get from env variable string, decode it from hex and write array as result
 /// ```
-/// const ID: [u8; 32] = env_to_array::hex_env_to_array!("_ENV_TO_ARRAY_HEX");
+/// env_to_array::test_compile_time_env!();
+/// const ID: [u8; 32] = env_to_array::hex_env_to_array!("__ENV_TO_ARRAY_HEX");
 /// ```
 #[proc_macro]
 pub fn hex_env_to_array(input: TokenStream) -> TokenStream {
